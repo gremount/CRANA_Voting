35dbfe548e0166108657776cc62beba383776817
wangshichao <wangshichao1030@gmail.com>
2015-11-08 14:53:02 +0800

    a little change

--- Voting and LP/main.cpp
+++ Voting and LP/main.cpp
@@ -4,24 +4,27 @@
 
 int main()
 {
+	srand((unsigned)time(NULL));
 	Graph g("d:\\github\\CRANA_Voting\\graph2.txt");
-	int caseN=6;
+	int caseN=10;
 	double judge=0,judge_sum=0;
 	int req_sum=0,result_sum=0;
-	string sAddress;
-	sAddress="d:\\github\\CRANA_Voting\\req1.txt";
+	
 	for(int icase=1;icase<=caseN;icase++)
 	{
 		//case输入
-		ifstream reqfile(sAddress);
-		sAddress[26]++;
 		int reqN,a,b,c;
 		vector<Req*> reqL;
-		reqfile>>reqN;
+		reqN=10;
 		reqL.clear();
 		for(int i=0;i<reqN;i++)
 		{
-			reqfile>>a>>b>>c;
+			a=0;b=0;c=0;
+			while(a==b || c==0){
+				a=rand() % (g.n-1)+1;
+				b=rand() % (g.n-1)+1;
+				c=rand() % MAXFLOW + 1;
+			}
 			Req* r=new Req(a,b,c);
 			reqL.push_back(r);
 		}
@@ -29,10 +32,12 @@ int main()
 		//最优部署计算
 		g.cost_best.clear();
 		g.cost_LP.clear();
-
+		//cout<<"35 line"<<endl;
 		for(int i=0;i<reqN;i++)
+		{
+			//cout<<reqL[i]->src<<" "<<reqL[i]->dst<<" "<<reqL[i]->flow<<endl;
 			g.cost_best.push_back(reqL[i]->flow * g.dijkstra(reqL[i]->src,reqL[i]->dst,reqL[i]->flow));
-
+		}
 
 		//线性规划部署
 		cout<<"--------------------------------------------------"<<endl;
@@ -51,6 +56,8 @@ int main()
 		else
 		{
 			judge=0;
+			//for(int i=0;i<reqN;i++)
+			//	cout<<g.cost_best[i]<<" "<<g.cost_LP[i]<<endl;
 			for(int i=0;i<reqN;i++)
 				judge += g.cost_best[i]/g.cost_LP[i];
 		}
